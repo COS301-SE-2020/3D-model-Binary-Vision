63ae4fc3ac3d355df3152cbb6762a34d6058085a
Jacobus1998 <jaco.fab4@gmail.com>
2020-06-10 14:24:12 +0200

    CSSApiReturned

--- API/server.js
+++ API/server.js
@@ -4,7 +4,8 @@ var express = require('express'),
     app = express(),
     port =process.env.PORT || 3000,
     mongoose =require('mongoose'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    serveStatic= require('serve-static');
 
 var MONGO_OPTIONS = {
   useNewUrlParser: true,
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost/ModelDB', MONGO_OPTIONS);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(serveStatic('webSite/'));
 
 var routes = require('./api/route/3DModelRoute');
 routes(app);
