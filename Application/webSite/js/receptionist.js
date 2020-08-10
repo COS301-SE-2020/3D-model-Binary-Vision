433d84581c6d5a0223ef7e1c23a27519b0e9144a
Jacobus1998 <jaco.fab4@gmail.com>
2020-08-10 21:25:40 +0200

    ReturnJSONObjectPatientsAndSavingReceptionistNotes

--- Application/webSite/js/receptionist.js
+++ Application/webSite/js/receptionist.js
@@ -1,4 +1,7 @@
+//Made by : Jacobus Janse van Rensburg
 
+// ==================================================================
+//function developed by : Jacobus Janse van Rensburg
 function initReceptionist()
 {
     //getReceptionist Info and populate visual info on page
@@ -9,11 +12,13 @@ function initReceptionist()
 }
 
 
-
+// ==================================================================
+//function developed by : Jacobus Janse van Rensburg
+//used to get the information of the receptionist and populate the html elements with data
 function getReceptionistInfo(){
     var response = fetch("/getReceptionist",{
         method:"POST",
-        header:{'Content-Type':'Application/json ; charset=UTF-8'}
+        headers:{'Content-Type':'Application/json ; charset=UTF-8'}
     });
 
     var info = [];
@@ -32,10 +37,40 @@ function getReceptionistInfo(){
     return info;
 }
 
-
+// ==================================================================
+//function developed by : Jacobus Janse van Rensburg
+//used to save receptionists notes to the database to be retrieved
 function saveNotes(){
     var noteSpace = document.getElementById('receptionistNotes').value;
     console.log("Note captured to save: "+ noteSpace);
     saveReceptionistNotes(noteSpace);
+}
 
+// ==================================================================
+//function developed by : Jacobus Janse van Rensburg
+//used as an api call to get all the patients that meet curtain search criteria
+function receptionistSearchPatient()
+{
+    var nameElement= document.getElementById("searchName");
+    var surnameElement = document.getElementById("searchSurname");
+    var idNumberElement = document.getElementById("searchPatientID");
+
+    var name , surname , idNumber;
+    if(nameElement!=null) name=nameElement.value;
+    if(surnameElement!=null) surname= surnameElement.value;
+    if(idNumberElement!=null) idNumber=idNumberElement.value;
+
+    var response = fetch("/searchPatient",{
+        method:"POST",
+        headers:{'Content-Type':'application/json; charset=UTF-8'},
+        body:JSON.stringify({ name,surname, idNumber })
+    });
+
+    response.then(res=> res.json().then(data=>{
+
+        for(var i in data){
+            console.log(data[i]);
+        }
+        
+    }));
 }
