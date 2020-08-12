9d00fbb97a7358a1fd0eb6e1905f10e1744efb5a
Steven Visser <stevenvisser99@gmail.com>
2020-08-12 12:01:34 +0200

    Updated timetable to be in 15 minute slots

--- Application/webSite/js/booking.js
+++ Application/webSite/js/booking.js
@@ -98,8 +98,17 @@ function populateCalander(data)
 // Function developed by: Jacobus Janse van Rensburg
 // Dynamically produces a schedule for the week for a doctor
 function createScheduler(overlay){
-    var days = ["mon","tue","wed","thu","fri","sat","sun"];
-    var times=[7,8,9,10,11,12,13,14,15,16,17,18];
+    var days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    var times=[
+               "09:00","09:15","09:30","09:45",
+               "10:00","10:15","10:30","10:45",
+               "11:00","11:15","11:30","11:45",
+               "12:00","12:15","12:30","12:45",
+               "13:00","13:15","13:30","13:45",
+               "14:00","14:15","14:30","14:45",
+               "15:00","15:15","15:30","15:45",
+               "16:00","16:15","16:30","16:45"
+              ];
     var replacement;
     replacement='<table class="table table-bordered" id="dayTable">';
     replacement+='<thead id="dayTableHead">';
@@ -127,7 +136,7 @@ function createScheduler(overlay){
     replacement += '</thead>'
     replacement+='<tbody>';
     for(var i = 0 ; i < times.length; i ++){
-        replacement+='<tr><td id="time">'+times[i]+':00'+'</td>';
+        replacement+='<tr><td id="time">'+times[i]+'</td>';
         for (var j =0 ; j < days.length; j ++)
         {
             replacement+='<td class="selectableTimeSlot" id="'+(currentDate+j)+'/'+currentMonth+'/'+currentYear+':'+times[i]+'" onclick="selectTime(\''+(currentDate+j)+'/'+currentMonth+'/'+currentYear+':'+times[i]+'\')"></td>';
