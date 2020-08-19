1fb21b39678707bee85a0a403fbf7ffdc8aa444b
RaniArraf <arraf.rani@gmail.com>
2020-08-19 10:45:02 +0200

    fixed the block display of the checkboxes
    made serach patient look easier to easy and become easier to use.

--- Application/webSite/js/booking.js
+++ Application/webSite/js/booking.js
@@ -54,7 +54,7 @@ function createDoctorsList(data)
     for(var i in data)
     {
       if(data != null){
-        replacement += "<div style='background-color: #003366; width: 300px; color: white; margin-left: auto; margin-right: auto; text-align: auto; border-radius: 5px;'>";
+        replacement += "<div style='background-color: #003366; width: 300px; color: white; margin-left: auto; margin-right: auto; text-align: auto; border-radius: 5px; display: inline-block;'>";
         replacement +='<br><li>Dr.'+data[i].surname+' ('+data[i].name+') </li><br><button class="btn btn-primary" onclick="selectDoctor(\''+data[i]._id+'\',\''+data[i].name+'\',\''+data[i].surname+'\')">Select</button> <br><br>';
         replacement += "</div>";
       }
@@ -93,7 +93,7 @@ function displayTimeTableOverlay()
         {
             for(var i in data)
             {
-                console.log("Bookins already made for dr: \n"+data[i]);
+                console.log("Bookings already made for Dr: \n"+data[i]);
             }
             populateCalander(data);
 
@@ -257,9 +257,9 @@ function createPatientSearchOverlay()
 
     overlay.innerHTML ='<div style="background-color:#003366;"><div id="doctorFormSignup"><br>';
     overlay.innerHTML+='<h2> Search Patient</h2><br>';
-    overlay.innerHTML+='<div class="change" class="custom-control"><input type="checkbox"  id="searchByName" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white;" for="searchByName"> Search By Name </label><br>';
-    overlay.innerHTML+='<input type="checkbox" id="searchBySurname" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white;" for="searchBySurname"> Search By Surname </label><br>';
-    overlay.innerHTML+='<input type="checkbox" id="searchByPatientID" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white;" for="searchByPatientID"> Search By Patient ID </label> <br><br></div>';
+    overlay.innerHTML+='<div class="change" class="custom-control"><input type="checkbox"  id="searchByName" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white; padding: 5px; left: 0px;" for="searchByName">  Search By Name </label><br>';
+    overlay.innerHTML+='<input type="checkbox" id="searchBySurname" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white; padding: 5px; left: 0px;" for="searchBySurname">  Search By Surname </label><br>';
+    overlay.innerHTML+='<input type="checkbox" id="searchByPatientID" name="searchBy" onclick="searchByInputDisplay()"><label class="form-check-label" style="color:white; padding: 5px; left: 0px;" for="searchByPatientID">  Search By Patient ID </label> <br><br></div>';
     overlay.innerHTML+='<div id="inputBoxes"></div><input style="margin-bottom: 20px;" class="btn btn-danger" type="submit" class="btn" value="Search" onclick="createPatientsListForBooking()"></div></div>';
 
 }
