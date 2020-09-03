53e5613dd045df4225ae46bb02dcbe735d94bd05
Jacobus1998 <jaco.fab4@gmail.com>
2020-09-03 19:16:01 +0200

    UploadMultipleImagesFixed

--- Application/api/controller/UploadController.js
+++ Application/api/controller/UploadController.js
@@ -1,28 +1,56 @@
-const { model } = require("mongoose");
 //file creates by: Jacobus Janse van Rensburg
 //controller for all the uploads and retrievals as well as the integrating of the c++ program 
 
-
+const formidable = require("formidable");
+const mongoose = require("mongoose");
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     
 
     uploadImages: function(req, res){
 
-        // get the images from the req files 
-        const images = req.files;
-        if (!images)
+
+        const form = formidable({ multiples: true });
+
+        form.parse(req, (err, fields, files) => 
         {
-            //if no images was sent we handle this by returning a error status 404
-            console.log("no images");
-            res.status(404);
-            return;
+            if(err)
+            {
+                console.log(err);
+                return;
+            }
+            const images = files;
+
+            if (!images)
+            {
+                //if no images was sent we handle this by returning a error status 404
+                res.status(404).send("no images");
+                return;
+            }
+            else {
+                //get image data from disk using path (use fs package)
+                //use the file paths to send the images to the c++ program to find the images
+                
+                //temp store file 
+                var dir=""+req.user+"-"+Date.now()
+                console.log(dir);
+                fs.mkdirSync(dir);
+                console.log(images);
+                for (let image of images["images[]"] ) {
+                    console.log("path: "+image.path+"\tname: "+image.name)
+                    fs.copyFileSync(image.path, path.join(dir, image.name));
+                }
+
+                //connect c++ program here and send file name when done c++ deletes file 
+
+                // res.send(images);
+                res.status(200);
+                return;
+            }
         }
-        else {
-            //there is images and we can do someting with them 
-            //just return them for now 
-            res.send(images);
-        }
+        );
     }
 }
