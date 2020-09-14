150e2dc344a29658d41dc87fc8bd8808fb6d01b4
MarcusWerren <MAWerren@gmail.com>
2020-09-14 18:18:04 +0200

    Minor updates to integration testing

--- Application/api/controller/3DModelController.js
+++ Application/api/controller/3DModelController.js
@@ -686,8 +686,7 @@ module.exports = {
       {
           if (err) 
           {
-              res.status(404)
-                .send("Error looking up doctor");
+              res.status(404).send("Error looking up doctor");
               return;
           } 
           else 
@@ -1198,10 +1197,12 @@ module.exports = {
             if(err)
             {
                 console.log(err);
+                res.status(401).send(err);
             }
             else
             {
                 console.log("Log successfully updated!");
+                res.status(200).send("Log successfully updated!");
             }
         });
         
@@ -1214,7 +1215,7 @@ module.exports = {
     {
         if(!req.user)
         {
-            res.status(401);
+            res.status(401).send("Unauthorized");
             return;
         }
 
@@ -1247,7 +1248,7 @@ module.exports = {
             {
                 if (err)
                 {
-                  res.status(400);
+                  res.status(400).send("Not created");
                   return;
                 }
                 res.status(201)
